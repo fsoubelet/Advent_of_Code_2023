2839747e2d2758d2d971fa50f214eb87a5b9eb14
Felix Soubelet <felix.soubelet@protonmail.com>
2023-12-02 12:09:21 +0100

    day 1

--- python/day_01/solution.py
+++ python/day_01/solution.py
@@ -1,95 +1,125 @@
 """
---- Intro ---
+--- Day 1: Trebuchet?! ---
 
-Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on Christmas.
-For that, their favorite snack is a special type of star fruit that only grows deep in the jungle.
-The Elves have brought you on their annual expedition to the grove where the fruit grows.
+Something is wrong with global snow production, and you've been selected to take a look.
+The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
 
-To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by December 25th.
-Although the Elves assure you that the grove has plenty of fruit, you decide to grab any fruit you see along the way, just in case.
+You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
 
 Collect stars by solving puzzles.
 Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first.
 Each puzzle grants one star. Good luck!
 
---- Part One ---
+You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
 
-The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot.
-As your boats approach land, the Elves begin taking inventory of their supplies.
-One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
+As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills.
+Consequently, the Elves are having trouble reading the values on the document.
 
-The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc. that they've brought with them, one item per line.
-Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
+The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover.
+On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 
-For example, suppose the Elves finish writing their items' Calories and end up with the following list:
+For example:
 
-1000
-2000
-3000
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
 
-4000
+In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
-5000
-6000
+Consider your entire calibration document. What is the sum of all of the calibration values?
 
-7000
-8000
-9000
-
-10000
-
-This list represents the Calories of the food carried by five Elves:
-- The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
-- The second Elf is carrying one food item with 4000 Calories.
-- The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
-- The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
-- The fifth Elf is carrying one food item with 10000 Calories.
-
-In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories.
-In the example above, this is 24000 (carried by the fourth Elf).
-
-Find the Elf carrying the most Calories.
-How many total Calories is that Elf carrying?
 
 --- Part Two ---
 
-By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
+Your calculation isn't quite right.
+It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
 
-To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories.
-That way, even if one of those Elves runs out of snacks, they still have two backups.
+Equipped with this new information, you now need to find the real first and last digit on each line.
+For example:
 
-In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories).
-The sum of the Calories carried by these three elves is 45000.
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
 
-Find the top three Elves carrying the most Calories.
-How many Calories are those Elves carrying in total?
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76.
+Adding these together produces 281.
+
+What is the sum of all of the calibration values?
 """
-from dataclasses import dataclass
 from pathlib import Path
 
+WORDS_TO_DIGITS = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
 
-@dataclass
-class Elf:
-    calories: int = 0
+
+def combine_line_first_last_digits_part_1(line: str) -> float:
+    """
+    Combine the first and last digits of the line.
+    Returns the corresponding number when combining the two digits.
+    """
+    first_digit = None
+    last_digit = None
+    for element in line:
+        if element.isdigit():
+            first_digit = element
+            break
+    for element in reversed(line):
+        if element.isdigit():
+            last_digit = element
+            break
+    return int(first_digit + last_digit)
 
 
-def elves_and_their_calories(inputs: list[str]) -> list[Elf]:
-    """Returns a list of Elf objects with their calories each, from the inputs."""
-    elves = []
-    current = Elf()
-    for line in inputs:
-        if line == "":
-            elves.append(current)
-            current = Elf()
-        else:
-            current.calories += int(line)
-    return elves
+def find_digit_including_words(line: str, reverse: bool = False) -> str:
+    """
+    Looks either in order or in reverse and gives the first (or corresponding to last, if in reverse)
+    digit encountered, be it a word or an actual digit. It is returned as a string.
+    """
+    word_text = ""
+    string_to_check = line[::-1] if reverse else line
+
+    for element in string_to_check:
+        # If we find a digit, it's done
+        if element.isdigit():
+            return element
+        # Otherwise we add the letter to the current "word" and will check if this word corresponds to a digit
+        word_text += element
+        for word in WORDS_TO_DIGITS:
+            check = word[::-1] if reverse else word
+            if check in word_text:
+                return WORDS_TO_DIGITS[word]
+
+
+def combine_line_first_last_digits_part_2(line: str) -> float:
+    """
+    Combine the first and last digits of the line, but digits spelled as words are also valid.
+    Returns the corresponding number when combining the two digits.
+    """
+    first_digit = find_digit_including_words(line)
+    last_digit = find_digit_including_words(line, reverse=True)
+    return int(first_digit + last_digit)
 
 
 if __name__ == "__main__":
     inputs = Path("input.txt").read_text().splitlines()
-    elves = elves_and_their_calories(inputs)
-    calories = [elf.calories for elf in elves]
-    top3_calories = sorted(calories, reverse=True)[:3]
-    print("Part 1: ", max(calories))
-    print("Part 2: ", sum(top3_calories))
+    # Part 1
+    combined_digits_as_numbers = [combine_line_first_last_digits_part_1(line) for line in inputs]
+    print(f"Part 1 answer: {sum(combined_digits_as_numbers)}")
+
+    # Part 2
+    combined_digits_as_numbers = [combine_line_first_last_digits_part_2(line) for line in inputs]
+    print(f"Part 2 answer: {sum(combined_digits_as_numbers)}")
